830d37852fb8d791e74d155618533a8a468a2555
aamir-sultan <asultan.here@gmail.com>
2023-05-28 06:29:46 +0500

    chore: setup for lsps {WIP}

---
+++ nvim/configs/lspconfigs.lua
@@ -0,0 +1,29 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "clangd"}
+
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--   }
+-- end
+
+-- Without the loop, you would have to manually set up each LSP 
+-- 
+-- lspconfig.html.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+--
+-- lspconfig.cssls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+
+lspconfig.svls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -420,5 +420,50 @@ local plugins = {
       },
     },
   },
+  -- Added for the setting up of the language server protocol settings. Add a language for installing its server protocol.
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     opts = {
+--       ensure_installed = {
+--         -- defaults 
+--         "vim",
+--         "lua",
+--
+--         -- web dev 
+--         -- "html",
+--         -- "css",
+--         -- "javascript",
+--         -- "typescript",
+--         -- "tsx",
+--         "json",
+--         -- "vue", "svelte",
+--
+--        -- low level
+--         "c",
+--         "rust"
+--         -- "zig"
+--       },
+--     },
+--   },
+--   -- In order to modify the `lspconfig` configuration:
+-- {
+--   "neovim/nvim-lspconfig",
+--    config = function()
+--       require "plugins.configs.lspconfig"
+--       require "custom.configs.lspconfig"
+--    end,
+-- },
+--
+--    {
+--    "williamboman/mason.nvim",
+--    opts = {
+--       ensure_installed = {
+--         "lua-language-server",
+--         "prettier",
+--         "stylua",
+--         "svls"
+--       },
+--     },
+--   },
 
 }return plugins
