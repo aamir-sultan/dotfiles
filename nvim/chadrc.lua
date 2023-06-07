7c913f12438873c56b83adace7bf504dad844d99
aamir-sultan <asultan.here@gmail.com>
2023-06-07 18:50:46 +0500

    chore::hammer: update the chadrc for supporting the statusline themes

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -1,6 +1,15 @@
 ---@type ChadrcConfig 
  local M = {}
- M.ui = {theme = 'nightfox'}
+ M.ui = {
+  theme = 'nightfox',
+  transparency = true,
+  statusline = {
+    -- theme = "vscode_colored"
+    -- theme = "vscode"
+    -- theme = "minimal"
+    theme = "default"
+  }
+}
  M.plugins = 'custom.plugins'
  M.mappings = require "custom.mappings"
  return M
