d326f99257baaca6cf494effb535bf5b30a08d0e
aamir-sultan <asultan.here@gmail.com>
2023-08-21 01:23:53 +0500

    chore: made the script paths dynamic

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
 
+# Check if the DOTFILES is not set
+if [ -z "$DOTFILES" ]; then
+# Set the DOTFILES path to this script.
+    DOTFILES="$(cd "$(dirname "$0")" && pwd)"
+fi
+
 # Setup the initial environment for the total setup
 source $DOTFILES/system/.variables
 
 #Set the path of the bashrc in the ~/.bashrc if already not exists otherwise print the information
-if grep -q 'source ~/dotfiles/bash/.bashrc'  ~/.bashrc; then
+if grep -q "source $DOTFILES/bash/.bashrc"  ~/.bashrc; then
     echo Path already set in $HOME/.bashrc
 else
     echo Setting the dotfiles bashrc path into ~/.bashrc    
@@ -13,11 +19,11 @@ fi
 
 #Set the path of the bashrc in the ~/.vimrc if already not exists otherwise print the information
 touch ~/.vimrc
-if grep -q 'source ~/dotfiles/vim/.vimrc'  ~/.vimrc; then
+if grep -q "source $DOTFILES/vim/.vimrc"  ~/.vimrc; then
     echo Path already set in $HOME/.vimrc
 else
     echo Setting the dotfiles vimrc path in ~/.vimrc
-    echo 'source ~/dotfiles/vim/.vimrc' >> ~/.vimrc
+    echo "source $DOTFILES/vim/.vimrc" >> ~/.vimrc
 fi
 # Dependencies installer script run
 #    source ./setup/dependencies_installer.sh
@@ -26,11 +32,11 @@ fi
 
 #Set the path of the bashrc in the ~/.vimrc if already not exists otherwise print the information
 touch ~/.tmux.conf
-if grep -q 'source-file ~/dotfiles/tmux/.tmux.conf'  ~/.tmux.conf; then
+if grep -q "source-file $DOTFILES/tmux/.tmux.conf"  ~/.tmux.conf; then
     echo Path already set in $HOME/.tmux.conf
 else
     echo Setting the dotfiles .tmux.conf path in ~/.tmux.conf
-    echo 'source-file ~/dotfiles/tmux/.tmux.conf' >> ~/.tmux.conf
+    echo "source-file $DOTFILES/tmux/.tmux.conf" >> ~/.tmux.conf
 fi
 
 # tmux plugin installer script run
