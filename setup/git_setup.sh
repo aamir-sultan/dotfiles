5dcc74f94c51b6c2b34e6d30ee9502035cf504c1
aamir-sultan <asultan.here@gmail.com>
2023-09-17 20:03:50 +0500

    chore: update the git setup scripts

--- setup/git_setup.sh
+++ setup/git_setup.sh
@@ -1,15 +1,55 @@
 #!/usr/bin/bash
-GIT_CONFIG=~/.gitconfig
 
-if [ -f "$GIT_CONFIG" ]; then
-    echo "$GIT_CONFIG exists."
-    if grep -qE "sclone|sup|spush" $GIT_CONFIG; then
-        echo Git aliases already set in $GIT_CONFIG. Nothing to do.
-    else
-        echo Setting the git aliases in $GIT_CONFIG 
-        cat $DOTFILES/git/.git_aliases >> $GIT_CONFIG  
-    fi
+GIT_CONFIG=~/.gitconfig
+IGNORE_FILE=".gitignore"
+IGNORE_FILE_PATH=$(realpath ${DOTFILES}/git)/$IGNORE_FILE
+touch $GIT_CONFIG
+
+echo "Working in the file: $GIT_CONFIG"
+
+if grep -q "\[include\]" $GIT_CONFIG; then
+  echo "[include] already set in $HOME/.gitconfig"
+  if grep -q "  path = $DOTFILES/git/.git_aliases" $GIT_CONFIG; then
+    echo Path for aliases file already set in $HOME/.gitconfig
+  else
+    echo Setting the dotfiles .git_aliases path in $GIT_CONFIG
+    echo "  path = $DOTFILES/git/.git_aliases" >> $GIT_CONFIG
+  fi
 else
-    echo Setting the git aliases in $GIT_CONFIG 
-    cat $DOTFILES/git/.git_aliases >> $GIT_CONFIG  
+  echo Setting the dotfiles .git_aliases path in $GIT_CONFIG
+  echo -e "\n[include]" >> $GIT_CONFIG
+  echo "  path = $DOTFILES/git/.git_aliases" >> $GIT_CONFIG
+fi
+
+if grep -q "\[core\]" $GIT_CONFIG; then
+echo "[core] already set in $HOME/.gitignore"
+  if grep -q "  exclude = $IGNORE_FILE_PATH" $GIT_CONFIG; then
+    echo Path for ignore file already set in $HOME/.gitconfig
+  else
+    # REPLACEMENT_TEXT="\tThis is the replacement text."
+    # Create a temporary file to store the output
+    TMP_FILE=$(mktemp)
+
+    readarray -t lines < "$GIT_CONFIG"
+    # Iterate over the lines in the array
+    for LINE in "${lines[@]}"; do
+
+      # Check if the LINE contains the "[core]" string
+      if grep -q "\[core\]" <<< $LINE; then
+
+        # If the LINE contains the "[core]" string, write the replacement text to the temporary file on the next LINE
+        echo "$LINE" >> "$TMP_FILE"
+        echo "  exclude = $IGNORE_FILE_PATH" >> "$TMP_FILE"
+      else
+        # If the LINE does not contain the "[core]" string, write it to the temporary file
+        echo "$LINE" >> "$TMP_FILE"
+      fi
+    done
+    # Move the temporary file to the original file
+    mv "$TMP_FILE" "$GIT_CONFIG"
+  fi
+else
+  echo Setting the dotfiles .git_aliases path in $GIT_CONFIG
+  echo -e "\n[core]" >> $GIT_CONFIG
+  echo "  exclude = $IGNORE_FILE_PATH" >> $GIT_CONFIG
 fi
