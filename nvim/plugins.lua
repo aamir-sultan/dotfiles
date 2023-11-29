7307963e43b49e164827024385743971503de75c
aamir-sultan <asultan.here@gmail.com>
2023-11-30 01:43:34 +0500

    chore: add ctrlp.vim for buufer management and fuzzy finding buffer

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -36,6 +36,7 @@ local plugins = {
   { 'ervandew/supertab' },
   { 'mg979/vim-visual-multi' },
   { 'christoomey/vim-tmux-navigator', lazy = true },
+  { 'ctrlpvim/ctrlp.vim', lazy = false },
   { 'MunifTanjim/nui.nvim' },
   {
     "numToStr/Comment.nvim",
@@ -316,71 +317,6 @@ local plugins = {
       -- vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
     end
   },
-  -- {
-  -- "nvim-neo-tree/neo-tree.nvim",
-  --   cmd = "Neotree",
-  --   keys = {
-  --     {
-  --       "<leader>fe",
-  --       function()
-  --         require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
-  --       end,
-  --       desc = "Explorer NeoTree (root dir)",
-  --     },
-  --     {
-  --       "<leader>fE",
-  --       function()
-  --         require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-  --       end,
-  --       desc = "Explorer NeoTree (cwd)",
-  --     },
-  --     { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
-  --     { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
-  --   },
-  --   deactivate = function()
-  --     vim.cmd([[Neotree close]])
-  --   end,
-  --   init = function()
-  --     vim.g.neo_tree_remove_legacy_commands = 1
-  --     if vim.fn.argc() == 1 then
-  --       local stat = vim.loop.fs_stat(vim.fn.argv(0))
-  --       if stat and stat.type == "directory" then
-  --         require("neo-tree")
-  --       end
-  --     end
-  --   end,
-  --   opts = {
-  --     filesystem = {
-  --       bind_to_cwd = false,
-  --       follow_current_file = true,
-  --       use_libuv_file_watcher = true,
-  --     },
-  --     window = {
-  --       mappings = {
-  --         ["<space>"] = "none",
-  --       },
-  --     },
-  --     default_component_configs = {
-  --       indent = {
-  --         with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-  --         expander_collapsed = "",
-  --         expander_expanded = "",
-  --         expander_highlight = "NeoTreeExpander",
-  --       },
-  --     },
-  --   },
-  --   config = function(_, opts)
-  --     require("neo-tree").setup(opts)
-  --     vim.api.nvim_create_autocmd("TermClose", {
-  --       pattern = "*lazygit",
-  --       callback = function()
-  --         if package.loaded["neo-tree.sources.git_status"] then
-  --           require("neo-tree.sources.git_status").refresh()
-  --         end
-  --       end,
-  --     })
-  --   end,
-  -- },
   -- { 'echasnovski/mini.animate', version = '*',
   --   init = function()
   --     require('mini.animate').setup()
@@ -419,50 +355,6 @@ local plugins = {
       },
     },
   },
-  -- Added for the setting up of the language server protocol settings. Add a language for installing its server protocol.
-  --   {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     opts = {
-  --       ensure_installed = {
-  --         -- defaults 
-  --         "vim",
-  --         "lua",
-  --
-  --         -- web dev 
-  --         -- "html",
-  --         -- "css",
-  --         -- "javascript",
-  --         -- "typescript",
-  --         -- "tsx",
-  --         "json",
-  --         -- "vue", "svelte",
-  --
-  --        -- low level
-  --         "c",
-  --         "rust"
-  --         -- "zig"
-  --       },
-  --     },
-  --   },
-  --   -- In order to modify the `lspconfig` configuration:
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --    config = function()
-  --       require "plugins.configs.lspconfig"
-  --       require "custom.configs.lspconfig"
-  --    end,
-  -- },
-  --
-  --    {
-  --    "williamboman/mason.nvim",
-  --    opts = {
-  --       ensure_installed = {
-  --         "lua-language-server",
-  --         "prettier",
-  --         "stylua",
-  --         "svls"
-  --       },
-  --     },
-  --   },
 
-}return plugins
+}
+return plugins
