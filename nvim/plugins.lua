3366bba21c4a99740fe50698abb94fcf8557658c
aamir-sultan <asultan.here@gmail.com>
2023-11-30 23:26:04 +0500

    chore: removed the delay time in tmux

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -86,13 +86,13 @@ local plugins = {
 
       -- If you want icons for diagnostic errors, you'll need to define them somewhere:
       vim.fn.sign_define("DiagnosticSignError",
-        {text = " ", texthl = "DiagnosticSignError"})
+      {text = " ", texthl = "DiagnosticSignError"})
       vim.fn.sign_define("DiagnosticSignWarn",
-        {text = " ", texthl = "DiagnosticSignWarn"})
+      {text = " ", texthl = "DiagnosticSignWarn"})
       vim.fn.sign_define("DiagnosticSignInfo",
-        {text = " ", texthl = "DiagnosticSignInfo"})
+      {text = " ", texthl = "DiagnosticSignInfo"})
       vim.fn.sign_define("DiagnosticSignHint",
-        {text = "", texthl = "DiagnosticSignHint"})
+      {text = "", texthl = "DiagnosticSignHint"})
       -- NOTE: this is changed from v1.x, which used the old style of highlight groups
       -- in the form "LspDiagnosticsSignWarning"
 
@@ -237,53 +237,53 @@ local plugins = {
               --"node_modules"
             },
             hide_by_pattern = { -- uses glob style patterns
-              --"*.meta",
-              --"*/src/*/tsconfig.json",
-            },
-            always_show = { -- remains visible even if other settings would normally hide it
-              --".gitignored",
-            },
-            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-              --".DS_Store",
-              --"thumbs.db"
-            },
-            never_show_by_pattern = { -- uses glob style patterns
-              --".null-ls_*",
-            },
+            --"*.meta",
+            --"*/src/*/tsconfig.json",
           },
-          follow_current_file = false, -- This will find and focus the file in the active buffer every
-          -- time the current file is changed while the tree is open.
-          group_empty_dirs = false, -- when true, empty folders will be grouped together
-          hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-          -- in whatever position is specified in window.position
-          -- "open_current",  -- netrw disabled, opening a directory opens within the
-          -- window like netrw would, regardless of window.position
-          -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-          use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
-          -- instead of relying on nvim autocmd events.
-          window = {
-            mappings = {
-              ["<bs>"] = "navigate_up",
-              ["."] = "set_root",
-              ["H"] = "toggle_hidden",
-              ["/"] = "fuzzy_finder",
-              ["D"] = "fuzzy_finder_directory",
-              ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
-              -- ["D"] = "fuzzy_sorter_directory",
-              ["f"] = "filter_on_submit",
-              ["<c-x>"] = "clear_filter",
-              ["[g"] = "prev_git_modified",
-              ["]g"] = "next_git_modified",
-            },
-            fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
-              ["<down>"] = "move_cursor_down",
-              ["<C-n>"] = "move_cursor_down",
-              ["<up>"] = "move_cursor_up",
-              ["<C-p>"] = "move_cursor_up",
-            },
-          },
+          always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+        --".DS_Store",
+        --"thumbs.db"
+      },
+      never_show_by_pattern = { -- uses glob style patterns
+      --".null-ls_*",
+    },
+  },
+  follow_current_file = false, -- This will find and focus the file in the active buffer every
+  -- time the current file is changed while the tree is open.
+  group_empty_dirs = false, -- when true, empty folders will be grouped together
+  hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+  -- in whatever position is specified in window.position
+  -- "open_current",  -- netrw disabled, opening a directory opens within the
+  -- window like netrw would, regardless of window.position
+  -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+  use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+  -- instead of relying on nvim autocmd events.
+  window = {
+    mappings = {
+      ["<bs>"] = "navigate_up",
+      ["."] = "set_root",
+      ["H"] = "toggle_hidden",
+      ["/"] = "fuzzy_finder",
+      ["D"] = "fuzzy_finder_directory",
+      ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+      -- ["D"] = "fuzzy_sorter_directory",
+      ["f"] = "filter_on_submit",
+      ["<c-x>"] = "clear_filter",
+      ["[g"] = "prev_git_modified",
+      ["]g"] = "next_git_modified",
+    },
+    fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+    ["<down>"] = "move_cursor_down",
+    ["<C-n>"] = "move_cursor_down",
+    ["<up>"] = "move_cursor_up",
+    ["<C-p>"] = "move_cursor_up",
+  },
+},
 
-          commands = {} -- Add a custom command or override a global one using the same function name
+commands = {} -- Add a custom command or override a global one using the same function name
         },
         buffers = {
           follow_current_file = true, -- This will find and focus the file in the active buffer every
@@ -323,37 +323,37 @@ local plugins = {
   --   end,
   -- },
   { 'echasnovski/mini.cursorword', version = '*',
-    init = function()
-      require('mini.cursorword').setup()
-    end,
-  },
-  { 'echasnovski/mini.indentscope', version = '*',
-    init = function()
-      require('mini.indentscope').setup()
-    end,
+  init = function()
+    require('mini.cursorword').setup()
+  end,
+},
+{ 'echasnovski/mini.indentscope', version = '*',
+init = function()
+  require('mini.indentscope').setup()
+end,
   },
   { 'phaazon/hop.nvim', lazy = true,
-    init = function()
-      require('hop').setup()
-    end,
+  init = function()
+    require('hop').setup()
+  end,
+},
+{ "nvim-tree/nvim-tree.lua", enabled = false,
+opts = {
+  git = {
+    enable = true,
   },
-  { "nvim-tree/nvim-tree.lua", enabled = false,
-    opts = {
-      git = {
-        enable = true,
-      },
-      renderer = {
-        highlight_git = true,
-        icons = {
-          show = {
-            git = true,
-          },
-        },
-      },
-      view = {
-        side = "right",
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
       },
     },
+  },
+  view = {
+    side = "right",
+  },
+},
   },
 
 }
