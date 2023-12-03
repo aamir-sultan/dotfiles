0e355ffbf8d04af44304b3e782e85c5ffe042f7e
aamir-sultan <asultan.here@gmail.com>
2023-12-04 02:47:08 +0500

    chore: updated the keymaping mechanism

--- nvim/lazyvim/lua/config/keymaps.lua
+++ nvim/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+function Map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
+
+-- Map("n", "<C-h>", "<C-w>h")
+-- Map("n", "<C-j>", "<C-w>j")
+-- Map("n", "<C-k>", "<C-w>k")
+-- Map("n", "<C-l>", "<C-w>l")
+--
+-- -- terminal
+-- Map("t", "<C-h>", "<cmd>wincmd h<CR>")
+-- Map("t", "<C-j>", "<cmd>wincmd j<CR>")
+-- Map("t", "<C-k>", "<cmd>wincmd k<CR>")
+-- Map("t", "<C-l>", "<cmd>wincmd l<CR>")
+
+-- Map("v", "J", ":m '>+1<CR>gv=gv")
+-- Map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
