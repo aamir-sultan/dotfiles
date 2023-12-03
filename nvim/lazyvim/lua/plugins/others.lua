02667330cffd9dc9ce7befa37280cb885bc3062d
aamir-sultan <asultan.here@gmail.com>
2023-12-04 00:09:49 +0500

    chore: fixed the tab shown wrong in LazyVim

--- nvim/lazyvim/lua/plugins/others.lua
+++ nvim/lazyvim/lua/plugins/others.lua
@@ -1,30 +1,31 @@
 return {
-  -- { "junegunn/fzf", run = ":call fzf#install()", lazy = false },
-  -- { "junegunn/fzf.vim", lazy = false },
-  {'BlakeJC94/alpha-nvim-fortune', lazy = true, event = "VimEnter", }, -- Is used for the quote in the start screen
-  { 'ThePrimeagen/vim-be-good', event = { "LazyFile", "VeryLazy" }, },
-  { 'frazrepo/vim-rainbow', lazy = true},
-  { 'tpope/vim-sensible', event = { "LazyFile", "VeryLazy" }, },
-  { 'tpope/vim-fugitive', event = { "LazyFile", "VeryLazy" }, },
-  { 'WeiChungWu/vim-SystemVerilog', event = { "LazyFile", "VeryLazy" }, },
-  { 'nachumk/systemverilog.vim', event = { "LazyFile", "VeryLazy" }, },
-  { 'airblade/vim-gitgutter', event = { "LazyFile", "VeryLazy" }, },
-  { 'tmux-plugins/vim-tmux', event = { "LazyFile", "VeryLazy" }, },
-  { 'djoshea/vim-autoread', event = { "LazyFile", "VeryLazy" }, },
-  { 'lambdalisue/nerdfont.vim', event = { "LazyFile", "VeryLazy" }, },
+  -- -- { "junegunn/fzf", run = ":call fzf#install()", lazy = false },
+  -- -- { "junegunn/fzf.vim", lazy = false },
+  { 'BlakeJC94/alpha-nvim-fortune',   lazy = true,                        event = "VimEnter", }, -- Is used for the quote in the start screen
+  { 'ThePrimeagen/vim-be-good',       event = { "LazyFile", "VeryLazy" }, },
+  { 'frazrepo/vim-rainbow',           lazy = true },
+  { 'tpope/vim-sensible',             event = { "LazyFile", "VeryLazy" }, },
+  { 'tpope/vim-fugitive',             event = { "LazyFile", "VeryLazy" }, },
+  -- { 'WeiChungWu/vim-SystemVerilog', event = { "LazyFile", "VeryLazy" }, },  -- Creating issues with tabs of LazyVim
+  { 'nachumk/systemverilog.vim',      event = { "LazyFile", "VeryLazy" }, },
+  { 'airblade/vim-gitgutter',         event = { "LazyFile", "VeryLazy" }, },
+  { 'tmux-plugins/vim-tmux',          event = { "LazyFile", "VeryLazy" }, },
+  { 'djoshea/vim-autoread',           event = { "LazyFile", "VeryLazy" }, },
+  { 'lambdalisue/nerdfont.vim',       event = { "LazyFile", "VeryLazy" }, },
   -- { 'ryanoasis/vim-devicons', event = { "LazyFile", "VeryLazy" }, },
-  { 'bagrat/vim-buffet', event = { "LazyFile", "VeryLazy" }, },
-  { 'vim-scripts/SearchComplete', event = { "LazyFile", "VeryLazy" }, },
-  { 'ervandew/supertab', lazy = true, },
-  { 'mg979/vim-visual-multi', event = { "LazyFile", "VeryLazy" }, },
+  { 'bagrat/vim-buffet',              event = { "LazyFile", "VeryLazy" }, },
+  { 'vim-scripts/SearchComplete',     event = { "LazyFile", "VeryLazy" }, },
+  { 'ervandew/supertab',              lazy = true, },
+  { 'mg979/vim-visual-multi',         event = { "LazyFile", "VeryLazy" }, },
   -- { 'christoomey/vim-tmux-navigator', event = { "LazyFile", "VeryLazy" }, },
   { 'christoomey/vim-tmux-navigator', event = { "LazyFile", "VeryLazy" }, },
-  { 'ctrlpvim/ctrlp.vim', lazy = true, event = "VimEnter", },
-  { 'nvim-zh/better-escape.vim', lazy = true, event = "VimEnter", },
+  { 'ctrlpvim/ctrlp.vim',             lazy = true,                        event = "VimEnter", },
+  { 'nvim-zh/better-escape.vim',      lazy = true,                        event = "VimEnter", },
   -- { 'MunifTanjim/nui.nvim', lazy = true },
   -- { 'beauwilliams/statusline.lua' },
   {
-    "numToStr/Comment.nvim", event = { "LazyFile", "VeryLazy" },
+    "numToStr/Comment.nvim",
+    event = { "LazyFile", "VeryLazy" },
     -- keys = { "gcc", "gbc" },
     -- init = function()
     --   require("core.utils").load_mappings "comment"
@@ -32,7 +33,7 @@ return {
     config = function(_, opts)
       require("Comment").setup(opts)
       local ft = require('Comment.ft')
-      ft.systemverilog = {'//%s', '/*%s*/'}
+      ft.systemverilog = { '//%s', '/*%s*/' }
     end,
   },
   -- { 'echasnovski/mini.animate', event = { "LazyFile", "VeryLazy" }, version = '*',
@@ -40,12 +41,18 @@ return {
   --     require('mini.animate').setup()
   --   end,
   -- },
-  { 'echasnovski/mini.cursorword', event = { "LazyFile", "VeryLazy" }, version = '*',
+  {
+    'echasnovski/mini.cursorword',
+    event = { "LazyFile", "VeryLazy" },
+    version = '*',
     init = function()
       require('mini.cursorword').setup()
     end,
   },
-  { 'echasnovski/mini.indentscope', event = { "LazyFile", "VeryLazy" }, version = '*',
+  {
+    'echasnovski/mini.indentscope',
+    event = { "LazyFile", "VeryLazy" },
+    version = '*',
     init = function()
       require('mini.indentscope').setup()
     end,
@@ -53,7 +60,7 @@ return {
   {
     -- "strash/everybody-wants-that-line.nvim", lazy = true,
     -- config = function()
-      -- [your configuration here]
+    -- [your configuration here]
     -- end
   }
 }
