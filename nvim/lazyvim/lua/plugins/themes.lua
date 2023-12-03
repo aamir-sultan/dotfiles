148445ef8de95a4c9ca1b896ad2317fd0c844bd8
aamir-sultan <asultan.here@gmail.com>
2023-12-04 03:12:12 +0500

    chore: udpated the themes list

--- nvim/lazyvim/lua/plugins/themes.lua
+++ nvim/lazyvim/lua/plugins/themes.lua
@@ -6,18 +6,21 @@ return {
   { "folke/tokyonight.nvim",    lazy = true, priority = 1000, opts = {}, },
   {
     "mcchrish/zenbones.nvim",
+    lazy = true,
     -- Optionally install Lush. Allows for more configuration or extending the colorscheme
     -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
     -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = { "rktjmp/lush.nvim" },
+    dependencies = { "rktjmp/lush.nvim", lazy = true },
     requires = { "rktjmp/lush.nvim" }
   },
-
+  { 'rebelot/kanagawa.nvim',       lazy = true },
+  { 'marko-cerovac/material.nvim', lazy = true },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      -- colorscheme = "gruvbox",
+      colorscheme = "kanagawabones",
       -- colorscheme = "nord",
       -- colorscheme = "noctu",
     },
