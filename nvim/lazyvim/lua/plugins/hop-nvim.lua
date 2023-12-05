cd5bec1f9d8a5fc25330070c81dc69823b0d3486
aamir-sultan <asultan.here@gmail.com>
2023-12-06 02:06:39 +0500

    chore: automated the animate plugin to work only on local

--- nvim/lazyvim/lua/plugins/hop-nvim.lua
+++ nvim/lazyvim/lua/plugins/hop-nvim.lua
@@ -1,6 +1,8 @@
 return {
   -- Better `movements`
-  { 'phaazon/hop.nvim', Verylazy = true,
+  {
+    'phaazon/hop.nvim',
+    Verylazy = true,
     opts = {
       -- Options would go here
     },
@@ -8,9 +10,9 @@ return {
       -- disable the keymap to grep files
       -- {"<leader>/", false},
       -- change a keymap
-      { "s", "<cmd> HopChar2<CR>", desc = "Find and Jump to two consecutive charachter" },
-      { "ss", "<cmd> HopChar1<CR>", desc = "Find and Jump to single charachter" },
-      { "sw", "<cmd> HopWord<CR>", desc = "Find and Jump to single word" },
+      { "s",  "<cmd> HopChar2<CR>",    desc = "Find and Jump to two consecutive charachter" },
+      { "ss", "<cmd> HopChar1<CR>",    desc = "Find and Jump to single charachter" },
+      { "sw", "<cmd> HopWord<CR>",     desc = "Find and Jump to single word" },
       { "sa", "<cmd> HopAnywhere<CR>", desc = "Find and where" },
     },
 
@@ -20,3 +22,4 @@ return {
     -- end,
   }
 }
+
--- nvim/lazyvim/lua/plugins/themes.lua
+++ nvim/lazyvim/lua/plugins/themes.lua
@@ -3,18 +3,19 @@ return {
   { 'ellisonleao/gruvbox.nvim', lazy = true, event = "VimEnter" },
   { 'arcticicestudio/nord-vim', lazy = true, event = "VimEnter" },
   { 'noahfrederick/vim-noctu',  lazy = true, event = "VimEnter" },
-  { "folke/tokyonight.nvim",    lazy = true, event = "VimEnter" , priority = 1000, opts = {}, },
+  { 'navarasu/onedark.nvim',  lazy = true, event = "VimEnter" },
+  -- { "folke/tokyonight.nvim",    lazy = true, event = "VimEnter" , priority = 1000, opts = {}, },
   {
     "mcchrish/zenbones.nvim",
     lazy = true,
-    event = "VimEnter", 
+    event = "VimEnter",
     -- Optionally install Lush. Allows for more configuration or extending the colorscheme
     -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
     -- In Vim, compat mode is turned on as Lush only works in Neovim.
     dependencies = { "rktjmp/lush.nvim", lazy = true, event = "VimEnter" },
     requires = { "rktjmp/lush.nvim" }
   },
-  { 'rebelot/kanagawa.nvim',       lazy = true, event = "VimEnter", opts = {theme = "dragon",} },
+  { 'rebelot/kanagawa.nvim',       lazy = true, event = "VimEnter", opts = { theme = "dragon", } },
   { 'marko-cerovac/material.nvim', lazy = true, event = "VimEnter" },
   -- Configure LazyVim to load gruvbox
   {
