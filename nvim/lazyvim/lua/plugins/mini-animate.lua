51558376b0cb949f8f61f7f1a16009ffb64cae5e
aamir-sultan <asultan.here@gmail.com>
2023-12-06 02:06:39 +0500

    chore: automated the animate plugin to work only on local

---
+++ nvim/lazyvim/lua/plugins/mini-animate.lua
@@ -0,0 +1,32 @@
+function isNotSSH()
+  local sshConnection = os.getenv("SSH_CONNECTION")
+  if sshConnection then
+    return true  -- Not an SSH connection
+  else
+    return false -- SSH connection detected
+  end
+end
+
+if isNotSSH() then
+  return {
+    {
+      'echasnovski/mini.animate', event = { "LazyFile", "VeryLazy" }, version = '*', enabled = false
+
+    }
+  }
+else
+  return {
+    {
+      'echasnovski/mini.animate',
+      event = { "LazyFile", "VeryLazy" },
+      version = '*',
+      init = function()
+        require('mini.animate').setup()
+      end,
+    },
+    -- {
+    --   'echasnovski/mini.animate', event = { "LazyFile", "VeryLazy" }, version = '*',
+    --
+    -- }
+  }
+end
--- nvim/lazyvim/lua/plugins/others.lua
+++ nvim/lazyvim/lua/plugins/others.lua
@@ -1,8 +1,9 @@
 return {
   -- { "junegunn/fzf", run = ":call fzf#install()", lazy = false },
   -- { "junegunn/fzf.vim", lazy = false },
-  { 'ThePrimeagen/vim-be-good',       event = { "LazyFile", "VeryLazy" }, }, -- Game for getting better at Vim
+  { 'm4xshen/hardtime.nvim',          event = { "LazyFile", "VeryLazy" }, }, -- Enable Transparent theming of different theme 
   { 'xiyaowong/transparent.nvim',     event = { "LazyFile", "VeryLazy" }, }, -- Enable Transparent theming of different theme
+  { 'ThePrimeagen/vim-be-good',       event = { "LazyFile", "VeryLazy" }, }, -- Game for getting better at Vim
   { 'frazrepo/vim-rainbow',           event = { "LazyFile", "VeryLazy" }, }, -- Add the indentation colors to different indentation level
   { 'wellle/targets.vim',             event = { "LazyFile", "VeryLazy" }, }, -- Provides additional targets for commands
   { 'tpope/vim-sensible',             event = { "LazyFile", "VeryLazy" }, }, -- Sensible mappings for Vim
@@ -11,19 +12,19 @@ return {
   { 'nachumk/systemverilog.vim',      event = { "LazyFile", "VeryLazy" }, }, -- Provides colors for System Verilog
   { 'airblade/vim-gitgutter',         event = { "LazyFile", "VeryLazy" }, }, -- Shows git diff markers in the sign column and stages/previews/undoes hunks and partial hunks.
   { 'preservim/tagbar',               event = { "LazyFile", "VeryLazy" }, }, -- Creates tags for files on fly
-  { 'ludovicchabant/vim-gutentags',   event = { "LazyFile", "VeryLazy" }, }, -- Creates tags for all files in the directory based on .sg or .git hooks or similar can be defined also. 
-  { 'tmux-plugins/vim-tmux',          event = { "LazyFile", "VeryLazy" }, },
-  { 'djoshea/vim-autoread',           event = { "LazyFile", "VeryLazy" }, },
-  { 'lambdalisue/nerdfont.vim',       event = { "LazyFile", "VeryLazy" }, },
-  -- { 'ryanoasis/vim-devicons', event = { "LazyFile", "VeryLazy" }, },
-  { 'bagrat/vim-buffet',              event = { "LazyFile", "VeryLazy" }, },
-  { 'vim-scripts/SearchComplete',     event = { "LazyFile", "VeryLazy" }, },
-  { 'ervandew/supertab',              lazy = true, },
+  { 'ludovicchabant/vim-gutentags',   event = { "LazyFile", "VeryLazy" }, }, -- Creates tags for all files in the directory based on .sg or .git hooks or similar can be defined also.
+  { 'tmux-plugins/vim-tmux',          event = { "LazyFile", "VeryLazy" }, }, -- Syntax highlighting and commentin for .tmux.conf file
+  { 'djoshea/vim-autoread',           event = { "LazyFile", "VeryLazy" }, }, -- Have Vim automatically reload a file that has changed externally
+  { 'lambdalisue/nerdfont.vim',       event = { "LazyFile", "VeryLazy" }, }, -- Fundemental plugin to handle Nerd Fonts in Vim
+  -- { 'ryanoasis/vim-devicons',        event = { "LazyFile", "VeryLazy" }, }, -- Adds file type icons to Vim plugins -- Is coverd by lambdalisue/nerdfont.vim
+  -- { 'bagrat/vim-buffet',             event = { "LazyFile", "VeryLazy" }, }, -- IDE-like Vim tabline -- This is covered by LazyVim
+  { 'vim-scripts/SearchComplete',     event = { "LazyFile", "VeryLazy" }, }, -- Tab completion of words inside of a search ('/')
   { 'mg979/vim-visual-multi',         event = { "LazyFile", "VeryLazy" }, },
-  -- { 'christoomey/vim-tmux-navigator', event = { "LazyFile", "VeryLazy" }, },
-  { 'christoomey/vim-tmux-navigator', event = { "LazyFile", "VeryLazy" }, },
-  { 'ctrlpvim/ctrlp.vim',             lazy = true,                        event = "VimEnter", },
-  { 'nvim-zh/better-escape.vim',      lazy = true,                        event = "VimEnter", },
+  { 'christoomey/vim-tmux-navigator', event = { "LazyFile", "VeryLazy" }, }, 
+  -- { 'gen740/SmoothCursor.nvim',       event = { "LazyFile", "VeryLazy" }, }, -- Add sub-cursor to show scroll direction
+  -- { 'ervandew/supertab',              lazy = true, event = "VimEnter", }, -- Having issues with it in neovim startup
+  { 'ctrlpvim/ctrlp.vim',             lazy = true, event = "VimEnter", },
+  { 'nvim-zh/better-escape.vim',      lazy = true, event = "VimEnter", },
   -- { 'MunifTanjim/nui.nvim', lazy = true },
   -- { 'beauwilliams/statusline.lua' },
   -- {
