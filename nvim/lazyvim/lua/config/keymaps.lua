936ddc019c6e42a5b51e41076386d3beaf129520
aamir-sultan <asultan.here@gmail.com>
2023-12-06 18:26:20 +0500

    chore: updated the keymap for neovim

--- nvim/lazyvim/lua/config/keymaps.lua
+++ nvim/lazyvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-function Map(mode, lhs, rhs, opts)
+function map(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
   if opts then
     options = vim.tbl_extend("force", options, opts)
@@ -10,19 +10,34 @@ function Map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, options)
 end
 
--- Map("n", "<C-h>", "<C-w>h")
--- Map("n", "<C-j>", "<C-w>j")
--- Map("n", "<C-k>", "<C-w>k")
--- Map("n", "<C-l>", "<C-w>l")
+-- map("n", "<C-h>", "<C-w>h")
+-- map("n", "<C-j>", "<C-w>j")
+-- map("n", "<C-k>", "<C-w>k")
+-- map("n", "<C-l>", "<C-w>l")
 --
 -- -- terminal
--- Map("t", "<C-h>", "<cmd>wincmd h<CR>")
--- Map("t", "<C-j>", "<cmd>wincmd j<CR>")
--- Map("t", "<C-k>", "<cmd>wincmd k<CR>")
--- Map("t", "<C-l>", "<cmd>wincmd l<CR>")
+-- map("t", "<C-h>", "<cmd>wincmd h<CR>")
+-- map("t", "<C-j>", "<cmd>wincmd j<CR>")
+-- map("t", "<C-k>", "<cmd>wincmd k<CR>")
+-- map("t", "<C-l>", "<cmd>wincmd l<CR>")
 
--- Map("v", "J", ":m '>+1<CR>gv=gv")
--- Map("v", "K", ":m '<-2<CR>gv=gv")
+-- map("v", "J", ":m '>+1<CR>gv=gv")
+-- map("v", "K", ":m '<-2<CR>gv=gv")
 
 -- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 -- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Fix * (Keep the cursor position, don't move to next match)
+map('n', '*', '*N', {desc = "Create new line below this line"} )
+
+-- leader-o/O inserts blank line below/above
+map('n', '<leader>o', 'o<ESC>', {desc = "Create new line below this line"})
+map('n', '<leader>O', 'O<ESC>', {desc = "Create new line below this line"})
+
+-- Mimic shell movements
+map('i', '<C-E>', '<C-o>$', {desc = "Jump to line End -- Mimic shell movement"})
+map('i', '<C-A>', '<C-o>^', {desc = "Jump to line Start -- Mimic shell movement"})
+
+-- Shortcut to yank register
+map({ 'n', 'x' }, '<leader>p', '"0p', {desc = "Paste from yank register"})
+
