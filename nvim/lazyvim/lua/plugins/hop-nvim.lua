7c66a89f1f90a1e972ee1148c8742db267adccda
aamir-sultan <asultan.here@gmail.com>
2023-12-08 00:33:57 +0500

    chore: udpated different settings

--- nvim/lazyvim/lua/plugins/hop-nvim.lua
+++ nvim/lazyvim/lua/plugins/hop-nvim.lua
@@ -1,25 +1,26 @@
+-- Since flash.nvim is already providing the same functionality. No need for newer Plugin.
 return {
-  -- Better `movements`
-  {
-    'phaazon/hop.nvim',
-    Verylazy = true,
-    opts = {
-      -- Options would go here
-    },
-    keys = {
-      -- disable the keymap to grep files
-      -- {"<leader>/", false},
-      -- change a keymap
-      { "s",  "<cmd> HopChar2<CR>",    desc = "Find and Jump to two consecutive charachter" },
-      { "ss", "<cmd> HopChar1<CR>",    desc = "Find and Jump to single charachter" },
-      { "sw", "<cmd> HopWord<CR>",     desc = "Find and Jump to single word" },
-      { "sa", "<cmd> HopAnywhere<CR>", desc = "Find and where" },
-    },
-
-    -- If activated this will not be lazy loaded.
-    -- init = function()
-    --   require('hop').setup(opts)
-    -- end,
-  }
+  -- -- Better `movements`
+  -- {
+  --   'phaazon/hop.nvim',
+  --   Verylazy = true,
+  --   opts = {
+  --     -- Options would go here
+  --   },
+  --   keys = {
+  --     -- disable the keymap to grep files
+  --     -- {"<leader>/", false},
+  --     -- change a keymap
+  --     { "hs",  "<cmd> HopChar2<CR>",    desc = "[H]op to two consecutive charachter" },
+  --     { "hs", "<cmd> HopChar1<CR>",    desc = "[H]op to single charachter" },
+  --     { "hw", "<cmd> HopWord<CR>",     desc = "[H]op to single word" },
+  --     { "ha", "<cmd> HopAnywhere<CR>", desc = "[H]op [A]nywhere" },
+  --   },
+  --
+  --   -- If activated this will not be lazy loaded.
+  --   -- init = function()
+  --   --   require('hop').setup(opts)
+  --   -- end,
+  -- }
 }
 
