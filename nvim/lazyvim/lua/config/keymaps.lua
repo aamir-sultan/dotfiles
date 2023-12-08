ed14b039b5ca5c17f7f7544987854d1b82c0c5f9
aamir-sultan <asultan.here@gmail.com>
2023-12-08 20:20:55 +0500

    chore: updated behaviour of qt and qf for buffer or vim close

--- nvim/lazyvim/lua/config/keymaps.lua
+++ nvim/lazyvim/lua/config/keymaps.lua
@@ -27,26 +27,27 @@ end
 -- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 -- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+-- Toggle Transparency
+Map("n", "<F7>", "<ESC><cmd>TransparentToggle<CR>", { desc = "Toggle Transparency" })
 
 -- Fix * (Keep the cursor position, don't move to next match)
-Map('v', 'ii', '<C-c>', {desc = "Exits to normal mode from visual"} )
+Map('v', 'ii', '<C-c>', { desc = "Exits to normal mode from visual" })
 -- vim.keymap.set("v", "ii", "<C-c>")
 
 -- Fix * (Keep the cursor position, don't move to next match)
-Map('n', '*', '*N', {desc = "Create new line below this line"} )
+Map('n', '*', '*N', { desc = "Create new line below this line" })
 
 -- leader-o/O inserts blank line below/above
-Map('n', '<leader>o', 'o<ESC>', {desc = "Create new line below this line"})
-Map('n', '<leader>O', 'O<ESC>', {desc = "Create new line below this line"})
+Map('n', '<leader>o', 'o<ESC>', { desc = "Create new line below this line" })
+Map('n', '<leader>O', 'O<ESC>', { desc = "Create new line below this line" })
 
 -- Mimic shell movements
-Map('i', '<C-E>', '<C-o>$', {desc = "Jump to line End -- Mimic shell movement"})
-Map('i', '<C-A>', '<C-o>^', {desc = "Jump to line Start -- Mimic shell movement"})
+Map('i', '<C-E>', '<C-o>$', { desc = "Jump to line End -- Mimic shell movement" })
+Map('i', '<C-A>', '<C-o>^', { desc = "Jump to line Start -- Mimic shell movement" })
 
 -- Shortcut to yank register
-Map({ 'n', 'x' }, '<leader>p', '"0p', {desc = "Paste from yank register"})
+Map({ 'n', 'x' }, '<leader>p', '"0p', { desc = "Paste from yank register" })
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 Map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
