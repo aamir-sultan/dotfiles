95b0ddc3635c2ebbab605db68ec7fe047259ec07
aamir-sultan <asultan.here@gmail.com>
2023-12-16 23:21:48 +0500

    chore: updated few plugins to be not installed automatically

--- nvim/lazyvim/lua/plugins/nvim-notify.lua
+++ nvim/lazyvim/lua/plugins/nvim-notify.lua
@@ -4,13 +4,18 @@ return {
     "rcarriga/nvim-notify",
     opts = {
       timeout = 3000,
-      render = "compact",
+      -- render = "default",
+      -- render = "compact",
+      render = "wrapped-compact",
       -- top_down = true,
       top_down = false,
-      stages = "slide",
+      -- stages = "fade",
+      stages = "fade_in_slide_out",
+      -- stages = "slide",
       level = 2,
       -- max_height = 0,
-      max_height = 1,
+      -- max_height = 1,
+      max_height = 2,
 
       -- max_height = function()
       --   return math.floor(vim.o.lines * 0.75)
@@ -18,9 +23,9 @@ return {
 
       -- Default value will be used from core config.
       -- max_width = 1,
-      -- max_width = function()
-      --   return math.floor(vim.o.columns * 0.75)
-      -- end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
       on_open = function(win)
         vim.api.nvim_win_set_config(win, { zindex = 100 })
       end,
