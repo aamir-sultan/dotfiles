f1cbb1ed428735458ff5f2df628e3ef83525b633
aamir-sultan <asultan.here@gmail.com>
2023-12-17 14:59:33 +0500

    chore: removed submitted tage

--- nvim/lazyvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lazyvim/lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "bash",
+        -- "bash",
         "json",
         "lua",
         -- "markdown",
