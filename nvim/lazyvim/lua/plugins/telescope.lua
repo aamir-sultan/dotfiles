c498cec145c9e8bd888a28d5ddb85f20eb09ec63
aamir-sultan <asultan.here@gmail.com>
2023-12-24 01:54:26 +0500

    chore: add entry changing with ctrl+jk and ctrl+u for telescope

--- nvim/lazyvim/lua/plugins/telescope.lua
+++ nvim/lazyvim/lua/plugins/telescope.lua
@@ -45,7 +45,14 @@ local options = {
     -- Developer configurations: Not meant for general override
     buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
     mappings = {
-      n = { ["q"] = require("telescope.actions").close },
+        i = {
+            ["<C-j>"] = require("telescope.actions").move_selection_next,
+            ["<C-k>"] = require("telescope.actions").move_selection_previous,
+            ['<C-u>'] = false,
+            },
+        n = {
+            ["q"] = require("telescope.actions").close
+            },
     },
   },
 
