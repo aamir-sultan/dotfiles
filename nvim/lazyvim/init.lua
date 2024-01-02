d8dfc77084b4a1352d1f32dc43c2ba64556867cb
aamir-sultan <asultan.here@gmail.com>
2024-01-02 19:38:22 +0500

    chore: update the vscode settings

--- nvim/lazyvim/init.lua
+++ nvim/lazyvim/init.lua
@@ -1,7 +1,7 @@
-if vim.g.vscode then
-    -- VSCode extension
-else
-    -- ordinary Neovim
+-- if vim.g.vscode then
+--     -- VSCode extension
+-- else
+-- ordinary Neovim
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-end
+-- end
