e6596580b8889ab6c4683fcf2b855ee0e00ddb21
aamir-sultan <asultan.here@gmail.com>
2024-03-13 01:55:45 +0500

    chore: fixed root dir issues

--- nvim/lazyvim/lua/config/options.lua
+++ nvim/lazyvim/lua/config/options.lua
@@ -21,3 +21,15 @@ opt.wrap = true
 
 vim.o.background = "dark" -- or "light" for light mode
 -- vim.cmd([[colorscheme gruvbox]])
+-- vim.cmd("cd %:p:h")
+-- vim.cmd("cd %:p")
+-- vim.o.autochdir = true
+
+-- Check if any arguments were passed to Neovim
+if #vim.fn.argv() > 0 then
+  -- A file was passed as an argument
+  vim.cmd('execute "cd " .. fnameescape(expand("%:p:h"))')
+else
+  -- No file was passed, use the current file's directory
+  vim.cmd('execute "cd " .. fnameescape(expand("%:p"))')
+end
