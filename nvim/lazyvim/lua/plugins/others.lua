d2897d348b84918cdbf0122f3239c4386769156f
aamir-sultan <asultan.here@gmail.com>
2024-03-13 01:55:45 +0500

    chore: fixed root dir issues

--- nvim/lazyvim/lua/plugins/others.lua
+++ nvim/lazyvim/lua/plugins/others.lua
@@ -1,15 +1,15 @@
 return {
   { 'ThePrimeagen/vim-be-good',     event = { "LazyFile" }, }, -- Game for getting better at Vim
   { 'frazrepo/vim-rainbow',         event = { "LazyFile" }, }, -- Add the indentation colors to different indentation level
-  { 'wellle/targets.vim',           event = { "LazyFile" }, },             -- Provides additional targets for commands
-  { 'tpope/vim-sensible',           event = { "LazyFile" }, },             -- Sensible mappings for Vim
-  { 'tpope/vim-fugitive',           event = { "LazyFile" }, },             -- Provides Git wrapper just like using git at terminal
-  { 'preservim/tagbar',             event = { "LazyFile" }, },             -- Creates tags for files on fly
-  { 'ludovicchabant/vim-gutentags', event = { "LazyFile" }, },             -- Creates tags for all files in the directory based on .sg or .git hooks or similar can be defined also.
-  { 'tmux-plugins/vim-tmux',        event = { "LazyFile" }, },             -- Syntax highlighting and commenting for .tmux.conf file
-  { 'djoshea/vim-autoread',         event = { "LazyFile" }, },             -- Have Vim automatically reload a file that has changed externally
+  { 'wellle/targets.vim',           event = { "LazyFile" }, }, -- Provides additional targets for commands
+  { 'tpope/vim-sensible',           event = { "LazyFile" }, }, -- Sensible mappings for Vim
+  { 'tpope/vim-fugitive',           event = { "LazyFile" }, }, -- Provides Git wrapper just like using git at terminal
+  { 'preservim/tagbar',             event = { "LazyFile" }, }, -- Creates tags for files on fly
+  { 'ludovicchabant/vim-gutentags', event = { "LazyFile" }, }, -- Creates tags for all files in the directory based on .sg or .git hooks or similar can be defined also.
+  { 'tmux-plugins/vim-tmux',        event = { "LazyFile" }, }, -- Syntax highlighting and commenting for .tmux.conf file
+  { 'djoshea/vim-autoread',         event = { "LazyFile" }, }, -- Have Vim automatically reload a file that has changed externally
   -- { 'lambdalisue/nerdfont.vim',     event = { "LazyFile", "VeryLazy" }, }, -- Fundemental plugin to handle Nerd Fonts in Vim
-  { 'vim-scripts/SearchComplete',   event = { "LazyFile" }, },             -- Tab completion of words inside of a search ('/')
+  { 'vim-scripts/SearchComplete',   event = { "LazyFile" }, }, -- Tab completion of words inside of a search ('/')
   { 'mg979/vim-visual-multi',       event = { "LazyFile" }, },
   -- {
   --   'echasnovski/mini.cursorword',
