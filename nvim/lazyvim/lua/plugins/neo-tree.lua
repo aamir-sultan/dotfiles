d97e3be23d39f26fe99fcc58bbd366fcf45250ca
aamir-sultan <asultan.here@gmail.com>
2024-03-28 15:35:24 +0500

    chore: reverted back to default settings

--- nvim/lazyvim/lua/plugins/neo-tree.lua
+++ nvim/lazyvim/lua/plugins/neo-tree.lua
@@ -33,11 +33,11 @@ local neotree_options = {
       --       --".null-ls_*",
       --     },
     },
-    follow_current_file = {
-      enabled = false,                      -- This will find and focus the file in the active buffer every time
-      --               -- the current file is changed while the tree is open.
-      leave_dirs_open = false,              -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-    },
+    -- follow_current_file = {
+    --   enabled = false,                      -- This will find and focus the file in the active buffer every time
+    --   --               -- the current file is changed while the tree is open.
+    --   leave_dirs_open = false,              -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+    -- },
     hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
     --   -- in whatever position is specified in window.position
     --   -- "open_current",  -- netrw disabled, opening a directory opens within the
