cbcd4b58ddd4582dfa824c891dc49e6a206416b3
aamir-sultan <asultan.here@gmail.com>
2024-05-22 01:11:49 +0500

    chore: removed

--- nvim/lazyvim/lua/plugins/others.lua
+++ nvim/lazyvim/lua/plugins/others.lua
@@ -4,7 +4,7 @@ return {
   { 'wellle/targets.vim',         event = { "LazyFile" }, },   -- Provides additional targets for commands
   { 'tpope/vim-sensible',         event = { "LazyFile" }, },   -- Sensible mappings for Vim
   { 'tpope/vim-fugitive',         event = { "LazyFile" }, },   -- Provides Git wrapper just like using git at terminal
-  { 'preservim/tagbar',           event = { "LazyFile" }, },   -- Creates tags for files on fly
+  -- { 'preservim/tagbar',           event = { "LazyFile" }, },   -- Creates tags for files on fly
   -- { 'ludovicchabant/vim-gutentags', event = { "LazyFile" }, }, -- Creates tags for all files in the directory based on .sg or .git hooks or similar can be defined also.
   { 'tmux-plugins/vim-tmux',      event = { "LazyFile" }, },   -- Syntax highlighting and commenting for .tmux.conf file
   { 'djoshea/vim-autoread',       event = { "LazyFile" }, },   -- Have Vim automatically reload a file that has changed externally
