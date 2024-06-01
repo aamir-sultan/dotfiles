996fed8bf4120c5e9a8457d466feadb72652b307
aamir-sultan <asultan.here@gmail.com>
2024-06-02 00:04:10 +0500

    chore: updated the cursor settings

---
+++ nvim/lazyvim/lua/plugins/smoothcursor.lua
@@ -0,0 +1,17 @@
+return {
+  'gen740/SmoothCursor.nvim',
+  event = "VeryLazy",
+  -- config = function()
+  --   require('smoothcursor').setup()
+  -- end,
+  opts = {
+    -- cursor = "▷",
+    -- cursor = "󰧂",
+    cursor = "",
+    -- cursor = "👉",
+    -- fancy = {
+    --     enable = false,
+    --     head = { cursor = "👉" },
+    --   },
+  }
+}
