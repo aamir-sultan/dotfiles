534ade9ff55461f935a22e66f9968753f1c11d35
aamir-sultan <asultan.here@gmail.com>
2024-08-19 02:59:10 +0500

    chore: fixed the conform notification for nvim version 0.10

--- nvim/lazyvim/lua/plugins/conform.lua
+++ nvim/lazyvim/lua/plugins/conform.lua
@@ -1,7 +1,5 @@
 return {
   "stevearc/conform.nvim",
-  -- tag = "nvim-0.9",
-  version = "nvim-0.9",
-  -- event = "VimEnter",
+  commit = "4e97712607bfdcadc097823339599e5bf05f97f9", -- This is done as the conform now ask for version 0.10 of nvim
   lazy = true,
-} 
+}
